e457850e67126a2b269b29ef6aab93935e5fc18b
zoonyoung <9851248@gmail.com>
2024-06-13 11:20:32 +0900

    feat: Loading스피너 구현

---
+++ src/components/Upload/LoadingSpinner.tsx
@@ -0,0 +1,23 @@
+import styles from "./LoadingSpinner.module.scss";
+
+export default function LoadingSpinner({ title }: { title: string }) {
+  return (
+    <div className={styles.loading}>
+      <div className={styles["lds-spinner"]}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+      <p className={styles.uploadingText}>{title}</p>
+    </div>
+  );
+}
